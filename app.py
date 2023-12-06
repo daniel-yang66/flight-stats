67a2b8ead6fa2726a5b0c900aa6c5e39121caf96
daniel-yang66 <daniel.yang787@outlook.com>
2023-12-05 23:18:15 -0500

    changes5

--- app.py
+++ app.py
@@ -16,31 +16,29 @@ load_figure_template('darkly')
 
 app.layout = dbc.Container([
     html.H2(id='title', children="Real-Time Scheduled & Active Flight Stats", style = {'text-align':'center','font-weight':'bold','font-size':35, 'font-family':'sans-serif'}),
-        dbc.Row(dcc.Link('Powered by aviationstack',href = 'https://aviationstack.com/'), style = {'text-align':'center','font-size':20}),
-    dbc.Row([
-        dbc.Col([
+    dbc.Row(dcc.Link('Powered by aviationstack',href = 'https://aviationstack.com/'), style = {'text-align':'center','font-size':20}),
+     dbc.Row([
+         dbc.Col([
         
-        html.H3('Origin Airport:', style={'margin-bottom':15, 'font-size':25, 'font-family':'sans-serif'}),
-        dcc.Input(id='dep',type='text', placeholder='Departure Airport', style = {'text-align':'center','border-radius': 10})]),
-        dbc.Col([
-        html.H3('Destination Airport:', style={'margin-bottom':15,'font-size':25, 'font-family':'sans-serif'}),
-        dcc.Input(id='arr', type='text', placeholder='Arrival Airport', style = {'text-align':'center','border-radius': 10})])], style = {'text-align':'center'}),
-        html.Br(),
+         html.H3('Origin Airport:', style={'margin-bottom':15, 'font-size':25, 'font-family':'sans-serif'}),
+         dcc.Input(id='dep',type='text', placeholder='Departure Airport', style = {'text-align':'center','border-radius': 11})]),
+         dbc.Col([
+         html.H3('Destination Airport:', style={'margin-bottom':15,'font-size':25, 'font-family':'sans-serif'}),
+         dcc.Input(id='arr', type='text', placeholder='Arrival Airport', style = {'text-align':'center','border-radius': 11})])], style = {'text-align':'center'}),
+         html.Br(),
         dbc.Row(
         html.Button('View Stats', id='submit', style = {'margin-bottom':10,'width':300, 'margin-left':420,'border-radius':30,'background-color':'green','color':'white'}), style = {'text-align':'center'}),
         dbc.Row([
         dbc.Col([
             dcc.Graph(id='pie')
-        ])])
+        ])
         
-    
+        ])
 ])
 
-@app.callback(Output("submit", "n_clicks"),Output('title','children'),Output('pie','figure'), Input('dep','value'), Input('arr','value'), Input('submit','n_clicks'))
-def view_stats(dep, arr, clicks):
-    if not clicks:
-        raise PreventUpdate
-
+@app.callback(Output('title','children'),Output('pie','figure'), Input('dep','value'), Input('arr','value'))
+def view_stats(dep,arr):
+    
     params = {
       'access_key': '349746e955fc67b11e41ece61dfad998',
         'dep_iata':dep,
@@ -56,12 +54,10 @@ def view_stats(dep, arr, clicks):
 
     response = requests.get('http://api.aviationstack.com/v1/flights', params)
     data = response.json()
-    response2 = requests.get('http://api.aviationstack.com/v1/flights', params2)
-    data2 = response2.json()
     
-    data_all = data['data'] + data2['data']
+    data_all = data['data']
     
-    title = f"Flight Stats | {dep.upper()} - {arr.upper()}"
+    title = f"Flight Stats {dep} | {arr}"
     
     airlines = []
     delayed = []
@@ -96,7 +92,6 @@ def view_stats(dep, arr, clicks):
                 
                 unique_flights.append(flight['flight']['number'])
                 airline = flight['airline']['name']
-                
                 delay = 0
                 if flight['departure']['actual'] != None and flight['departure']['scheduled'] < flight['departure']['actual']:
                     delay += (datetime.strptime(flight['departure']['actual'][0:10] + ' '+ flight['departure']['actual'][11:19], '%Y-%m-%d %H:%M:%S') - datetime.strptime(flight['departure']['actual'][0:10] + ' '+ flight['departure']['actual'][11:19], '%Y-%m-%d %H:%M:%S')).total_seconds() / 60
@@ -113,10 +108,7 @@ def view_stats(dep, arr, clicks):
 
     df = pd.DataFrame(list(zip(airlines, delayed,iata)), columns=['Airline','Delay Status','Count'])
     figure = px.pie(df.groupby('Airline').count().reset_index(),values='Count',names='Airline', hole = 0.7, title='Airline Market Share')
-    clicks = None
-    return clicks,title, figure
-    
+    return title, figure
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
-
+    app.run_server(debug=True)
